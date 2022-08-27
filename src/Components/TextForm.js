a4de62ee2061a5cd17ea4c25e05e71c8665f5066
Aryaja Nair <aryajanair313@gmail.com>
2022-08-27 16:01:51 +0530

    TextForm alignment changes

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -31,26 +31,30 @@ const [text, setText] = useState('Enter your text here......');
 
   return (
     <>
-    <div className='container border' >
+    <div className='container ' >
             <h1>{props.heading}</h1>
 
             <div className="mb-3 ">
             {/*<label for="myBox" className="form-label">Example textarea</label>*/}
                 <textarea className="form-control" id="myBox" value = {text} rows="8" onChange={handleOnChange} ></textarea>
             </div>
-
+          <div className='d-flex justify-content-start'>
             <button type="button" className="btn btn-success mx-2 " onClick={handleUpClick} >Convert to upper case</button>
             <button type="button" className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lower case</button>
             <button type="button" className="btn btn-danger mx-2" onClick={handleCleClick}>Clear Text</button>
-    
+          </div>
     </div>
 
-    <div className='container my-3'>
-      <h3> Your Text Summary</h3>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
-      <p>{0.008 *text.split(" ").length }Minutes to read</p>
+    <div className='container my-3 d-flex align-items-start flex-column '>
+
+      <div className='container  d-flex align-items-start flex-column border'>
+        <h3> Your Text Summary</h3>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 *text.split(" ").length }Minutes to read</p>
+      </div>
+
       <h3>Preview</h3>
-      <p>{text}</p>
+      <p className='text-left'>{text}</p>
     </div>
     </>
   )
